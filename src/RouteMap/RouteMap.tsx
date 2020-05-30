763cb7afac3234607c5a8750b2c542db397b06e5
harunari <nanteneus@hotmail.co.jp>
2020-05-30 10:42:10 +0000

    fix routing function like leaflet routing machine tutorial's

--- src/RouteMap/RouteMap.tsx
+++ src/RouteMap/RouteMap.tsx
@@ -5,20 +5,21 @@ import * as React from 'react';
 import { LatLng, LeafletMouseEvent, Routing } from 'leaflet';
 import { PinValue } from './Pin';
 import RoutingLayer from './Routing';
+import L = require('leaflet');
 
 interface RouteMapState {
-    pins: PinValue[];
-    isMapInit: boolean;
+    clickedPosition: L.LatLng | null;
+    routingControl: L.Routing.Control | null;
 }
 
 export default class RouteMap extends React.Component<{}, RouteMapState> {
-    private mockKey = 1;
     private map: LeafMap | null = null;
 
-    constructor(prop: { }) {
+    constructor(prop: {}) {
         super(prop);
         this.state = {
-            pins: [], isMapInit: false
+            clickedPosition: null,
+            routingControl: null
         };
     }
 
@@ -31,23 +32,51 @@ export default class RouteMap extends React.Component<{}, RouteMapState> {
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors" />
-                {this.state.isMapInit && <RoutingLayer map={this.map!} />}
+                {this.map !== null                    
+                    && <RoutingLayer
+                        map={this.map}
+                        clickedPosition={this.state.clickedPosition}
+                        routingControl={this.state.routingControl!} />}
             </LeafMap>
         );
     }
 
     private saveMap = (map: LeafMap): void => {
+        
         this.map = map;
+
         this.setState({
-            pins: this.state.pins,
-            isMapInit: true
+            routingControl: this.getDefaultControl(this.map.leafletElement, [])
         });
     }
 
     private handleClick(mouseEvent: LeafletMouseEvent): void {
 
-        const pins = this.state.pins.slice();
-        pins.push({ key: this.mockKey++, pos: mouseEvent.latlng });
-        this.setState({ pins });
+        if (this.map === null) { return; }
+
+        this.setState({
+            clickedPosition: mouseEvent.latlng,
+            routingControl: this.state.routingControl
+        });
+    }
+
+    private getDefaultControl(map: L.Map, waypoints: L.LatLng[]): L.Routing.Control {
+        return L.Routing.control({
+            waypoints: waypoints,
+            router: new L.Routing.OSRMv1({
+                serviceUrl: 'http://127.0.0.1:5000/route/v1'
+            }),
+            lineOptions: {
+                styles: [
+                    {
+                        color: "blue",
+                        opacity: 0.6,
+                        weight: 4
+                    }
+                ]
+            },
+            fitSelectedRoutes: false,
+            showAlternatives: false
+        }).addTo(map);
     }
 }
--- src/RouteMap/Routing.tsx
+++ src/RouteMap/Routing.tsx
@@ -1,32 +1,61 @@
-import { MapLayer, MapLayerProps, withLeaflet } from 'react-leaflet';
+import { MapLayer, MapLayerProps, withLeaflet, MapProps } from 'react-leaflet';
 import * as L from 'leaflet'
 import 'leaflet-routing-machine';
 import { Map as LeafMap } from 'react-leaflet';
+import RouteMap from './RouteMap';
 
-class RoutingLayer extends MapLayer<{ map: LeafMap } & MapLayerProps, L.Layer> {
+type RoutingLayerProp = {
+    map: LeafMap,
+    clickedPosition: L.LatLng | null,
+    routingControl: L.Routing.Control
+} & MapLayerProps;
+
+class RoutingLayer extends MapLayer<RoutingLayerProp, L.Layer> {
+
     public createLeafletElement(_: unknown): L.Layer {
-        const { map } = this.props;
-        let leafletElement = L.Routing.control({
-            router: new L.Routing.OSRMv1({
-                serviceUrl: 'http://127.0.0.1:5000/route/v1'
-            }),
-            lineOptions: {
-                styles: [
-                    {
-                        color: "blue",
-                        opacity: 0.6,
-                        weight: 4
-                    }
-                ]
-            },
-            fitSelectedRoutes: false,
-            showAlternatives: false
-        }).addTo(map.leafletElement)
-        .setWaypoints([
-            L.latLng(34, 135),
-            L.latLng(139, 49)
-        ]);
-        return leafletElement.getPlan();
+        const { routingControl } = this.props;
+
+        if (routingControl === null) { throw new Error('routing control is not initialized.'); }
+
+        return routingControl.getPlan();
+    }
+
+    public updateLeafletElement(_: unknown, prop: RoutingLayerProp) {
+        const { map, clickedPosition, routingControl } = this.props;
+        
+        if (clickedPosition === null) { return routingControl.getPlan(); }
+
+        const container = L.DomUtil.create('div'),
+            startBtn = this.createButton('Start from this location', container),
+            destinationBtn = this.createButton('Go to this location', container);
+
+        L.popup()
+            .setContent(container)
+            .setLatLng(clickedPosition)
+            .openOn(map.leafletElement);
+
+        const newWaypoint: L.Routing.Waypoint = {
+            latLng: clickedPosition
+        }
+
+        L.DomEvent.on(startBtn, 'click', () => {
+            routingControl.spliceWaypoints(0, 1, newWaypoint);
+            map.leafletElement.closePopup();
+        });
+
+        L.DomEvent.on(destinationBtn, 'click', () => {
+            routingControl.spliceWaypoints(routingControl.getWaypoints().length - 1, 1, newWaypoint);
+            map.leafletElement.closePopup();
+        });
+
+        return routingControl.getPlan();
+    }
+
+    private createButton(label: string, container: HTMLElement) {
+        var btn = L.DomUtil.create('button', '', container);
+        btn.setAttribute('type', 'button');
+        btn.innerHTML = label;
+        return btn;
     }
 }
 
