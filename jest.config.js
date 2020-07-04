bc8d6e1a5a8b8b33d2afe820e0d84824947875db
e a <nanteneus@hotmail.co.jp>
2020-07-04 09:20:25 +0000

    Modify jest config

--- jest.config.js
+++ jest.config.js
@@ -22,8 +22,6 @@ module.exports = {
             "jest-junit",
             {
                 "suiteName": "jest tests",
-                "outputDirectory": "reports/jest",
-                "outputName": "js-test-results.xml",
                 "classNameTemplate": "{classname}-{title}",
                 "titleTemplate": "{classname}-{title}",
                 "ancestorSeparator": " › "
